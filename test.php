68cb67a78b0439904231972a2e102bd85833b0e4
Spencer Lewson <slewson@calpoly.edu>
2012-11-04 00:09:19 -0700

    test hi

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
+<?php $thisPage="test"; ?>
 <?php include 'page_header.php'; ?>
 
 <b>Sub-Page Title</b><p>
 
 Here is the specific content of this page....<br>
+
+<?php if (thisPage == "test")
+{
+	echo "<p> hi </p>";
+}
 
 <?php include 'page_footer.php'; ?>
